36568a50822a1a42b9b4d9a9bb39a1d96105699d
palakgoyal2015 <124762359+palakgoyal2015@users.noreply.github.com>
2024-03-22 22:31:29 +0530

    Add files via upload

---
+++ Invalidcrimenumber.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class Invalidcrimenumber extends Exception{
+    public Invalidcrimenumber(){
+
+    }
+    public Invalidcrimenumber(String msg){
+        super(msg);
+    }
+}
