34978a76859da33495fc1b0615ad63db2d90c792
palakgoyal2015 <124762359+palakgoyal2015@users.noreply.github.com>
2024-03-24 20:25:44 +0530

    Update criminalpersondao.java

--- criminalpersondao.java
+++ criminalpersondao.java
@@ -4,4 +4,5 @@ public interface criminalpersondao {
     public String registercriminalperson( int crimePlaceId,int criminalNumber, String name, int age, String gender, String address,
     String identifyingMark, String areaofArrest);
     public String addSuspectCountColumn();
+    public int getSuspectCountForCrimePlace(int crimePlaceId);
 }
